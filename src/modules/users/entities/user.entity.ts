38a166befddfb9450f75a1c6c8667b92109a08ab
VoDinhNghia <vodinhnghia85@gmail.com>
2023-03-14 17:42:23 +0700

    Update UserEntity

--- src/modules/users/entities/user.entity.ts
+++ src/modules/users/entities/user.entity.ts
@@ -5,6 +5,33 @@ export class UserEntity {
   @PrimaryGeneratedColumn()
   id: number;
 
-  @Column({ length: 25 })
-  fullName: string;
+  @Column({ length: 100 })
+  email?: string;
+
+  @Column()
+  firstName: string;
+
+  @Column()
+  lastName: string;
+
+  @Column()
+  middleName: string;
+
+  @Column()
+  userId?: string;
+
+  @Column()
+  profileId?: string;
+
+  @Column()
+  passWord?: string;
+
+  @Column()
+  avatar?: string; // link url BE
+
+  @Column()
+  status?: string;
+
+  @Column()
+  role?: string;
 }
