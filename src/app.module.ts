976f75b219209f62b199249149556161d552feb0
VoDinhNghia <vodinhnghia85@gmail.com>
2023-03-15 08:14:20 +0700

    setup file .env and add autoLoadEntities

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,18 +5,25 @@ import { ScheduleModule } from '@nestjs/schedule';
 import { AppService } from './app.service';
 import { AppController } from './app.controller';
 import { CronjobModule } from './modules/cronjob/cronjob.module';
-
+import { ConfigModule, ConfigService } from '@nestjs/config';
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: 'root',
-      database: 'mgt_library',
-      entities: [],
-      synchronize: true,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `.env`,
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        type: 'mysql',
+        host: config.get('MYSQL_HOST'),
+        port: parseInt(config.get('MYSQL_PORT')),
+        username: config.get('MYSQL_USER'),
+        password: config.get('MYSQL_PASS'),
+        database: config.get('MYSQL_DATABASE'),
+        synchronize: true,
+        autoLoadEntities: true,
+      }),
     }),
     ScheduleModule.forRoot(),
     UsersModule,
