6bdd14bf9eb33e07efb323523e05c22a35d87272
VoDinhNghia <vodinhnghia85@gmail.com>
2023-03-16 10:17:45 +0700

    Migrate data user and update code sync data

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -1,19 +1,41 @@
-import { Controller, Get, Logger, Res, UseGuards, Query } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Logger,
+  Res,
+  Req,
+  UseGuards,
+  Query,
+  Post,
+} from '@nestjs/common';
 import { ApiBearerAuth, ApiResponse, ApiTags } from '@nestjs/swagger';
-import { ErolesUser } from 'src/constants/constant';
+import { ErolesUser, keyAccessBackend } from 'src/constants/constant';
 import { ResponseRequest } from 'src/utils/response-api';
 import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
 import { RoleGuard } from '../auth/guards/role-auth.guard';
 import { UsersService } from './users.service';
-import { Response } from 'express';
+import { Response, Request } from 'express';
 import { UserResponseDto } from './dtos/users.response.dto';
 import { UserQueryDto } from './dtos/users.query.dto';
+import { AuthServiceAccessByKey } from 'src/validates/validate.service.key-access';
 
-@Controller('users')
+@Controller('api/users')
 @ApiTags('users')
 export class UsersController {
   private readonly logger = new Logger(UsersService.name);
   constructor(private service: UsersService) {}
+
+  @Post('/migrate') // only one migrate then Block and user cronjob service
+  @UseGuards(AuthServiceAccessByKey(keyAccessBackend))
+  async migrateUsersData(
+    @Res() res: Response,
+    @Req() req: Request,
+  ): Promise<ResponseRequest> {
+    this.logger.log('api migrate users');
+    const { body = [] } = req;
+    await this.service.migrateUserFromBackend(body);
+    return new ResponseRequest(res, true, 'Get user list success.');
+  }
 
   @Get()
   @ApiBearerAuth()
--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { CommonException } from 'src/exceptions/exeception.common-error';
 import { Repository } from 'typeorm';
 import { UserQueryDto } from './dtos/users.query.dto';
 import { UserResponseDto } from './dtos/users.response.dto';
@@ -11,6 +12,35 @@ export class UsersService {
     @InjectRepository(Users)
     private usersRepository: Repository<Users>,
   ) {}
+
+  async migrateUserFromBackend(users = []): Promise<void> {
+    try {
+      await this.usersRepository.clear();
+      const userDto = users.map((user: any) => {
+        const dto = {
+          email: user?.email,
+          firstName: user?.profile?.firstName,
+          lastName: user?.profile?.lastName,
+          middleName: user?.profile?.middleName,
+          userId: user?._id,
+          code: user?.profile?.code,
+          profileId: user?.profile?._id,
+          passWord: user?.passWord,
+          avatar: user?.profile?.avatar,
+          status: user?.status,
+          role: user?.role,
+          mobile: user?.profile?.mobile,
+        };
+        return dto;
+      });
+      const results = this.usersRepository.create(userDto);
+      await this.usersRepository.save(results);
+      console.log('Sync data success!');
+    } catch {
+      console.log('Sync data failed!');
+      new CommonException(500, 'Can not sync data.');
+    }
+  }
 
   async getUsers(
     queryDto: UserQueryDto,
